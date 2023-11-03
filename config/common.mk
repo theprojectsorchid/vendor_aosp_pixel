33a7dc6be76232d7a131e6d368704f6a9d887c17
theprojectsorchid <126561359+theprojectsorchid@users.noreply.github.com>
2023-11-03 13:44:03 +0000

    Revert "Orchidos QS : qs tile customise"
    This reverts commit 19fb97c47f8550a7f9e875cc41026ea66960bd52.

--- config/common.mk
+++ config/common.mk
@@ -234,6 +234,4 @@ $(call inherit-product, vendor/aosp/config/lowram.mk)
 # OrchidOs packages
 $(call inherit-product, vendor/aosp/config/themeorc.mk)
 
-# OrchidOs packages
-$(call inherit-product, vendor/aosp/config/qstile.mk)
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
